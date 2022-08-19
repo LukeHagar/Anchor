2ffe9fa0921da9e1619c3d38fd68372ffa19c0a9
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2022-08-19 12:20:20 -0500

    Formatting and Bumping Updates

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 /*global chrome*/
-import logo from "./logo.svg";
 import "./App.css";
+import logo from "./logo.svg";
 
 import * as React from "react";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
-import PropTypes from "prop-types";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import Paper from "@mui/material/Paper";
-import CardContent from "@mui/material/CardContent";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
 import {
   Button,
   Checkbox,
@@ -31,6 +20,17 @@ import {
   Tooltip,
   Zoom,
 } from "@mui/material";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Paper from "@mui/material/Paper";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import Typography from "@mui/material/Typography";
+import PropTypes from "prop-types";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
